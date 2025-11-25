45d07f20848998da47f1d7e0f726314c5268703a
prda5207 <piya95@outlook.com>
2025-11-25 17:45:33 +0100

    dictionaries

--- demo_dict_movies.py
+++ demo_dict_movies.py
@@ -1,4 +1,10 @@
 #dictionaries are UNORDERED unique keys , create, grow and shrink
 # from python 3.6, dict keys are INSERTION ORDER
+import pprint
 
-movies =
+movies =  {'eric': ['pulp fiction', 'forest gump', 'platoon'],
+'bertwin': ['kill bill', 'hateful 8', 'the firm'],
+'stephan': ['james bond', 'Bourne', 'batman']
+ }
+
+print(movies)
