0d98fb39228fb632a06720f0fe63ca3b6d7535fa
piyadalal <piya95@outlook.com>
2025-11-25 11:00:46 +0000

    Day 2 : Understanding flow control : added while - else

--- demo_pin.py
+++ demo_pin.py
@@ -7,13 +7,20 @@
 """
 master_pin = "0123"
 pin = None
+attempts = 0
 
-while pin != master_pin:
+while pin != master_pin and attempts < 3:
     pin = input("enter pin = ")
     if pin == master_pin:
         print("valid pin")
+        break # correct master pin breaks out
     else:
         print("invalid pin")
+        attempts += 1   # allow more attempts until pin not correct
+
+else:
+    # only executes ONCE when while loop becomes False
+    print("Too  many attempts, card is blocked")
 
 
 print("Done")
