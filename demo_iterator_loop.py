2d716ec9218999e9084e885e2f8fe8af4063a50c
piyadalal <piya95@outlook.com>
2025-11-25 11:38:11 +0000

    Day 2 : enumerate

--- demo_iterator_loop.py
+++ demo_iterator_loop.py
@@ -9,3 +9,21 @@ students = ["a", "b", "c", "d"]
 for name in students:
     print(name, end="/n")
     print(name, end=",")
+
+# Iterate through the list and modify each object
+# using iterators for loops and index
+# enumerate returns tuple as (index_no, object: list, str ) ()
+idx = 0
+for name in students:
+    print(name.upper())
+    students[idx] = name.upper()
+    idx += 1
+
+
+#enumrate removes need for idx and idx increment
+# enumerate returns a tuple immutable tube of idx, name
+for (idx,name) in enumerate(students):
+    if idx not in (1,2):
+        print(name.upper())
+        students[idx] = name.upper()
+print("Students =", students) # same as putting in else and 2 spaces not required as part of loop
