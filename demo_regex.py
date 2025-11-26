02d04767b376f0bf39bae834435f82cad3481910
piyadalal <piya95@outlook.com>
2025-11-26 10:19:20 +0000

    regex demo python file

--- demo_regex.py
+++ demo_regex.py
@@ -1,4 +1,12 @@
+import re
+
 fh_in = open(r"C://labs//words", mode="rt")
 for line in fh_in:
     if line.startswith("Y") and line.rstrip("\n").endswith("n") and "town" in line:
+        print(line, end="")
+
+
+for line in fh_in:
+    m = re.search("^ring$")
+    if m:
         print(line, end="")
