3dffd68936596846f5e71088ce2e0f6c5e36a9cc
piyadalal <piya95@outlook.com>
2025-11-27 11:57:30 +0000

    adv.py in Calculator package

---
+++ Calculator/app/adv.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python3
+# Author: DCameron
+# Description: This module defines some ADVANCE calculator
+# functions
+#Advance Calculator App with power, modulus and square root functions
+import sys
+
+def add (*args):
+    """Return SUM of ALL parameters"""
+    sum = 0
+    for num in args:
+        sum += num
+    return float(sum)
+
+def mul (*args):
+    """Return product of ALL parameters"""
+    product = 1
+    for num in args:
+        product *= num
+    return float(product)
+
+def div (x,z):
+    """Return quotient of x divided b z to 3 decimal places parameters"""
+    return round(x/z, 3)
+
+print("********** Basic Calc App ************")
+print(f"4 + 3 + 2 + 1 = {add(4, 3, 2, 1)}")
+print(f"4 * 3 * 2 = {mul(4, 3, 2)}")
+print(f"4 / 3 = {div(4, 3)}")
+print("***************************************")
+
+sys.exit(0) # Explicit EXIT with return code (0=success, 1-255=error)
